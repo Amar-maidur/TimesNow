7b69e9e5a008262ca3763a65ac73601acf2ab76c
unknown <amar.maidur@digitalshark.in>
2022-06-13 10:33:21 +0530

    my project

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="a">
+      <Link to="/"></Link>
+      <Link to="/create"></Link>
+    </nav>
+  );
+};
+
+export default Navbar;
